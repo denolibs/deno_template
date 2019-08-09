5f6e2808edc159b1171bfda9fa96c54d8b8066d8
JD Davison <joshua@davison.asia>
2019-08-09 18:10:49 +1000

    Updated the executable batch script

--- run.bat
+++ run.bat
@@ -1,8 +1,15 @@
 :<<WINDOWS
   @echo off
+  SETLOCAL
+  Set DEBUG=*
   IF [%JUSTTERMINATE%] == [OKAY] (
     SET JUSTTERMINATE=
-    PowerShell.exe -Command "$env:DEBUG='*'; deno run --allow-env .\lib\mod.ts"
+      desno.exe version >NUL 2>&1 && (
+        deno.exe run --allow-env .\lib\mod.ts %*
+      ) || (
+        echo 'deno.exe' is not installed, or could not be found in your %%PATH%%
+        echo See https://deno.land/x/install/README.md for installation instructions.
+      )
   ) ELSE (
     SET JUSTTERMINATE=OKAY
     CALL %0 %* <NUL
@@ -11,16 +18,25 @@
 WINDOWS
 
 #<<OTHEROS
-  DEBUG=* deno run --allow-env ./lib/mod.ts
+  if which deno > /dev/null
+    then
+      DEBUG=* deno run --allow-env ./lib/mod.ts $@
+    else
+      echo "'deno' is not installed, or could not be found in your \$PATH"
+      echo "See https://deno.land/x/install/README.md for installation instructions."
+    fi
   exit;
 OTHEROS
 
-:///////////////////////////////////////////////////////////////
-://                                                           //
-:// Note: This is a multi-OS script to load our Deno module.  //
-:// This file need to be marked executable on Linux/Mac.      //
-:// $ chmod +x run.bat                                ~OzJD   //
-://                                                           //
-:// P.S. Windows IF avoids the "Terminate batch job (Y/N)?"   //
-://                                                           //
-:///////////////////////////////////////////////////////////////
+:////////////////////////////////////////////////////////////////
+://                                                            //
+://  Note: This is a multi-OS script to load our Deno module.  //
+://  Arguments are passed to Deno, and environment variable    //
+://  DEBUG is set to '*' for the debug module.                 //
+://                                                            //
+://  This file needs to be marked executable on Linux/Mac.     //
+://  $ chmod +x run.bat                           ~OzJD 2019   //
+://                                                            //
+://  P.S. This avoids "Terminate batch job (Y/N)?" on Windows  //
+://                                                            //
+:////////////////////////////////////////////////////////////////
